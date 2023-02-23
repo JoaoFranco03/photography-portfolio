995fb7661ab71127f066b21e38fb04d1a9c7c971
João Franco <joaopedrocostinha1982@gmail.com>
2023-02-23 01:05:15 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# photographer-portfolio
+ A Website created using Tailwind CSS, HTML, CSS and JavaScript that can be used as a Photographer Portfolio.
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
