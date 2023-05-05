08d37d14475f220a780862575fdab379cd6498aa
Linyi Li <linyili@andrew.cmu.edu>
2023-05-04 22:38:05 -0400

    update dependency

--- dist/gulpfile.js
+++ dist/gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 
 gulp.task('copy', function() {
-  return gulp.src('../node_modules/flowbite/dist/flowbite.min.js')
+  return gulp.src('../node_modules/flowbite/dist/flowbite.min.js',
+  '../node_modules/flowbite/dist/flowbite.min.js.map')
     .pipe(gulp.dest('./'));
 });
