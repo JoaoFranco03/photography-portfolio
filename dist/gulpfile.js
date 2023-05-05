da5101a33c2faa001c4a281ee67c5c13c8347587
Linyi Li <linyili@andrew.cmu.edu>
2023-05-04 22:39:17 -0400

    update min.js

--- dist/gulpfile.js
+++ dist/gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 
 gulp.task('copy', function() {
-  return gulp.src('../node_modules/flowbite/dist/flowbite.min.js',
-  '../node_modules/flowbite/dist/flowbite.min.js.map')
+  return gulp.src(['../node_modules/flowbite/dist/flowbite.min.js',
+  '../node_modules/flowbite/dist/flowbite.min.js.map'])
     .pipe(gulp.dest('./'));
 });
